0e10c7674f63d13c766efae59572baec1b5a22e8
magic891103 <133174388+magic891103@users.noreply.github.com>
2023-08-06 20:06:45 +0000

    second try

--- src/models.py
+++ src/models.py
@@ -10,23 +10,23 @@ Base = declarative_base()
 class Follower(Base):
     __tablename__ = 'follower'
     id = Column(Integer, primary_key=True)
-    user_from_id =(Integer,ForeignKey('user.id'))
-    user_to_id =(Integer,ForeignKey('user.id'))
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
 
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    username = Column(String(250),nullable=False)
+    username = Column(String(250), nullable=False)
     firstname = Column(String(250))
     lastname = Column(String(250))
     email = Column(String(250))
 
 class Comment(Base):
-    __tablename__='comment'
+    __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
     comment_text = Column(String(800))
-    post_id=(Integer,ForeignKey('post.id'))
-    author_id=(Integer,ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    author_id = Column(Integer, ForeignKey('user.id'))
 
 class Post(Base):
     __tablename__ = 'post'
@@ -39,8 +39,8 @@ class Media(Base):
     url = Column(String(600))
     post_id = Column(Integer, ForeignKey('post.id'))
 
-def to_dict(self):
-    return {}
+    def to_dict(self):
+        return {}
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
